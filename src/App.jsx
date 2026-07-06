b99b70fba0bf9dfb1263697978131c80d1e20f1b
roc-ket-cod-er <gargm0325@wrdsb.ca>
2026-07-06 16:48:55 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
+import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 import { loadStops } from "./loadStops";
 
-const stops = loadStops();
-
 export default function App() {
+  const [stops, setStops] = useState([]);
+
+  useEffect(() => {
+    loadStops().then((data) => {
+      setStops(data);
+    });
+  }, []);
+
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <MapContainer
-        center={[43.4723, -80.5449]}
-        zoom={14}
+        center={[43.4643, -80.5204]}
+        zoom={13}
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {stops.map((stop) => (
-          <Marker key={stop.id} position={[stop.lat, stop.lng]}>
-            <Popup>{stop.name}</Popup>
+          <Marker
+            key={stop.id}
+            position={[stop.lat, stop.lng]}
+          >
+            <Popup>
+              {stop.name}
+            </Popup>
           </Marker>
         ))}
+
       </MapContainer>
     </div>
   );
