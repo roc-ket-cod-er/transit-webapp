5df75b73ec68aa19db289a1f0a6c96d6d4694714
roc-ket-cod-er <gargm0325@wrdsb.ca>
2026-07-07 14:09:16 -0400

    Updated and streamlines code
    Consolidated multiple functions into one.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { loadGRTStops, loadGuelphTransitStops } from "./loadStops";
+import { loadStops } from "./loadStops";
 
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
@@ -10,38 +10,30 @@ delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
   shadowUrl: markerShadow,
 });
 
 export default function App() {
   const [stops, setStops] = useState([]);
-  const [stops2, setStops2] = useState([]);
 
   useEffect(() => {
-    loadGRTStops().then((data) => {
-      console.log(data.length);
-      console.log(data.slice(0, 5));
+    loadStops().then((data) => {
       setStops(data);
-    });
-    loadGuelphTransitStops().then((data) => {
-      console.log(data.length);
-      console.log(data.slice(0, 5));
-      setStops2(data);
     });
   }, []);
 
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <MapContainer
-        center={[43.4643, -80.5204]}
-        zoom={13}
+        center={[43.4643, -80.52]}
+        zoom={10}
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
           attribution="&copy; OpenStreetMap contributors"
           url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
         {stops.map((stop) => (
           <Marker
             key={`${stop.id}-${stop.lat}-${stop.lng}`}
@@ -52,17 +44,6 @@ export default function App() {
             </Popup>
           </Marker>
         ))}
-        {stops2.map((stop) => (
-          <Marker
-            key={`${stop.id}-${stop.lat}-${stop.lng}`}
-            position={[stop.lat, stop.lng]}
-          >
-            <Popup>
-              {stop.name}
-            </Popup>
-          </Marker>
-        ))}
-
       </MapContainer>
     </div>
   );
--- src/loadStops.js
+++ src/loadStops.js
@@ -1,28 +1,27 @@
 import Papa from "papaparse";
 
-export async function loadGRTStops() {
-  const response = await fetch("/gtfs/grt/stops.txt");
-  const text = await response.text();
+const transitAgencies = [
+  "GRT",
+  "Guelph Transit",
+  "Stratford Transit"
+];
 
-  const result = Papa.parse(text, {
-    header: true,
-    skipEmptyLines: true
-  });
+export async function loadStops() {
+  const allStops = [];
 
-  return result.data
-    .filter((stop) => stop.stop_id && stop.stop_lat && stop.stop_lon)
-    .map((stop) => ({
-      id: stop.stop_id,
-      name: stop.stop_name,
-      lat: Number(stop.stop_lat),
-      lng: Number(stop.stop_lon)
-    }));
+  for (const agency of transitAgencies) {
+    const stops = await loadStopsForAgency(agency);
+    allStops.push(...stops);
+    console.log(`Loaded ${stops.length} stops for ${agency}`);
+  }
+
+  return allStops;
 }
 
-export async function loadGuelphTransitStops() {
-  const response = await fetch("/gtfs/guelph-transit/stops.txt");
+async function loadStopsForAgency(agency) {
+  const path = `/gtfs/${agency}/stops.txt`;
+  const response = await fetch(path.toLowerCase().replace(' ', '-'));
   const text = await response.text();
-
   const result = Papa.parse(text, {
     header: true,
     skipEmptyLines: true
